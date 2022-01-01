4601524ee8a62fbef3773def8e8ee41fb6747eb9
Arsalan <arsalan.rk8@gmail.com>
2022-01-01 19:26:31 +0530

    Code CleanUp

--- app/src/main/java/com/arsalan/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/arsalan/bookhub/fragment/DashboardFragment.kt
@@ -39,45 +39,14 @@ class DashboardFragment : Fragment() {
     lateinit var recyclerDashboard: RecyclerView
     lateinit var layoutManager: RecyclerView.LayoutManager
 
-    lateinit var btnCheckInternet:Button
 
     lateinit var progressLayout: RelativeLayout
     lateinit var progressBar: ProgressBar
 
     val  bookInfoList = arrayListOf<Book>()
 
-/*
-//    val bookList = arrayListOf(
-//        "P.S. I Love You",
-//        "The Great Gatsby",
-//        "Anna Karenina",
-//        "Madame Bovary",
-//        "War and Peace",
-//        "Lolita",
-//        "The Adventure of Huckleberry Finn",
-//        "Moby-Dick",
-//        "The Lord Of The Rings"
-//    )
-
- */
 
     lateinit var recyclerAdapter: DashboardRecyclerAdapter
-/*
-//    val bookInfoList= arrayListOf<Book>(
-//        Book("P.S. I love You", "Cecelia Ahern", "Rs. 299", "4.5", R.drawable.ps_ily),
-//        Book("The Great Gatsby", "F. Scott Fitzgerald", "Rs. 399", "4.1", R.drawable.great_gatsby),
-//        Book("Anna Karenina", "Leo Tolstoy", "Rs. 199", "4.3", R.drawable.anna_kare),
-//        Book("Madame Bovary", "Gustave Flaubert", "Rs. 500", "4.0", R.drawable.madame),
-//        Book("War and Peace", "Leo Tolstoy", "Rs. 249", "4.8", R.drawable.war_and_peace),
-//        Book("Lolita", "Vladimir Nabokov", "Rs. 349", "3.9", R.drawable.lolita),
-//        Book("Middlemarch", "George Eliot", "Rs. 599", "4.2", R.drawable.middlemarch),
-//        Book("The Adventures of Huckleberry Finn", "Mark Twain", "Rs. 699", "4.5", R.drawable.adventures_finn),
-//        Book("Moby-Dick", "Herman Melville", "Rs. 499", "4.5", R.drawable.moby_dick),
-//        Book("The Lord of the Rings", "J.R.R Tolkien", "Rs. 749", "5.0", R.drawable.lord_of_rings)
-//    )
-
-
- */
 
 
 
@@ -90,43 +59,11 @@ class DashboardFragment : Fragment() {
         //view is used because it is built in oncreatview
         recyclerDashboard=view.findViewById(R.id.recyclerDashboard)
 
-        btnCheckInternet=view.findViewById(R.id.btnCheckInternet)
         progressLayout=view.findViewById(R.id.progressLayout)
         progressBar=view.findViewById(R.id.progressBar)
 
         progressLayout.visibility= View.VISIBLE
 
-        btnCheckInternet.setOnClickListener {
-            if (ConnectionManager().checkConnectivity(activity as Context)) {
-                // Internet is available
-                val dialog =  AlertDialog.Builder(activity as Context)
-                dialog.setTitle("Success")
-                dialog.setMessage("Internet Connection Found")
-                dialog.setPositiveButton("ok"){
-                    text , listner->// do nothing
-                }
-                dialog.setNegativeButton("Cancel"){
-                    text,listner->
-                    //do nothing
-                }
-                dialog.create()
-                dialog.show()
-            } else {
-                //Internet is not available
-                val dialog =  AlertDialog.Builder(activity as Context)
-                dialog.setTitle("Error")
-                dialog.setMessage("Internet Connection not Found")
-                dialog.setPositiveButton("ok"){
-                        text , listner->// do nothing
-                }
-                dialog.setNegativeButton("Cancel"){
-                        text,listner->
-                    //do nothing
-                }
-                dialog.create()
-                dialog.show()
-            }
-        }
 
         //activity- cos doing it in fragment not activity
         layoutManager=LinearLayoutManager(activity)
@@ -169,12 +106,7 @@ class DashboardFragment : Fragment() {
 
                                 recyclerDashboard.layoutManager = layoutManager
 
-                                recyclerDashboard.addItemDecoration(
-                                    DividerItemDecoration(
-                                        recyclerDashboard.context,
-                                        (layoutManager as LinearLayoutManager).orientation
-                                    )
-                                )
+
                             }
                         } else {
                             Toast.makeText(
@@ -194,6 +126,7 @@ class DashboardFragment : Fragment() {
 
                     // here we will handle the errors
                     Toast.makeText(activity as Context, " volley error occurred", Toast.LENGTH_SHORT).show()
+
 
 
 
